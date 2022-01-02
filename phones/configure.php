8e23b18c2daf7f1b17826dacfeb7a9d0378ca4ed
Daniel Bispo <szagot@gmail.com>
2022-01-02 05:37:12 -0300

    Modificado BD pra prod

--- phones/configure.php
+++ phones/configure.php
@@ -10,10 +10,10 @@ return [
     'tokenExpires' => '+6 hours',
 
     // Dados do BD
-    'db_name' => 'id17127412_phones',
+    'db_name' => '342570',
     'db_host' => 'localhost',
-    'db_user' => 'id17127412_uphones',
-    'db_pass' => 'Ph0n3s!20222',
+    'db_user' => '342570',
+    'db_pass' => 'DSpider1981',
 
     // Dados para phones
     'country_code' => '+55',
